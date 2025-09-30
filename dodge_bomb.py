ab3f7b506e2e5078b2027f6442d8252a95cea56a
C0C24021 <c0c240216a@edu.teu.ac.jp>
2025-09-30 14:37:59 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,12 +1,24 @@
 import os
+import random
 import sys
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1100, 650
+DELTA={
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(5,0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
+def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH <rct.right:
+        yoko = False
+    if rct.left < 0 or HEIGHT <rct.bottom:
+        tate = False
+    return yoko,tate
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -14,6 +26,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))  # 空のサーフェイス
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10) #  爆弾四隅消した
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0,WIDTH)  # 爆弾座標
+    bb_rct.centery = random.randint(0,HEIGHT)  # 爆弾座標
+    vx, vy = +5, +5 #  爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -24,16 +43,29 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
